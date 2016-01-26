a7c2e9d50d796ae13ebbd82679a2938eb9362280
Trav <travis@Traviss-MacBook-Air.local>
2016-01-25 19:48:10 -1000

    boot and title
    boot state and title state done

---
+++ game.js
@@ -0,0 +1,71 @@
+var game;
+var bgColors = [0xF16745, 0xFFC65D, 0x7BC8A4, 0x4CC3D9, 0x93648D, 0x7c786a,
+0x588c73, 0x8c4646, 0x2a5b84, 0x73503c];
+
+window.onload = function() {	
+	game = new Phaser.Game(640, 960, Phaser.AUTO, ""); // add Game object
+	game.state.add("Boot", boot);
+	game.state.add("Preload", preload);
+	game.state.add("TitleScreen", titleScreen);
+	game.state.add("PlayGame", playGame);
+	game.state.add("GameOverScreen", gameOverScreen);
+	game.state.start("Boot");
+}
+
+var boot = function(game){};
+boot.prototype = {
+	preload: function(){
+		this.game.load.image("loading","assets/sprites/loading.png");
+	},
+	create: function(){
+		game.scale.pageAlignHorizontally = true;
+		game.scale.pageAlignVertically = true;
+		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		console.log("game started");
+		game.state.start("Preload");
+	}
+}
+
+var preload = function(game){};
+preload.prototype = {
+	preload: function(){
+		var loadingBar = this.add.sprite(game.width/2, game.height/2,"loading");
+		loadingBar.anchor.setTo(0.5);
+		//setPreloadSprite(loadingBar);" turns and image into a 
+		//loading bar which grows as assets are being loaded.
+		game.load.setPreloadSprite(loadingBar); 
+		game.load.image("title","assets/sprites/title.png");
+		game.load.image("playbutton", "assets/sprites/playbutton.png");
+	},
+	create: function(){
+		game.state.start("TitleScreen");
+	}
+}
+
+var titleScreen = function(game){};
+titleScreen.prototype = {
+	create: function(){
+		//set random background color.
+		game.stage.backgroundColor = bgColors[game.rnd.between(0,bgColors.length-1)];
+		var title = game.add.image(game.width/2, 210, "title");
+		title.anchor.set(0.5);
+		var playButton = game.add.button(game.width/2, game.height-150,"playbutton", this.startGame);
+		playButton.anchor.set(0.5);
+	},
+	startGame: function(){
+		game.state.start("PlayGame");
+	}
+}
+
+var playGame = function(game){};
+playGame.prototype = {
+	create: function(){
+		console.log("play the game");
+	}
+}
+
+var gameOverScreen = function(game){};
+gameOverScreen.prototype = {
+	create: function(){
+	}
+}
