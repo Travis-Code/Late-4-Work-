98dc7f87b464869624ecb0472057cfe8586992a0
Trav <travis@Traviss-MacBook-Air.local>
2016-01-26 18:51:52 -1000

    titleScreen titleBG.tint
    added titleScreen tint

--- game.js
+++ game.js
@@ -36,6 +36,7 @@ preload.prototype = {
 		game.load.setPreloadSprite(loadingBar); 
 		game.load.image("title","assets/sprites/title.png");
 		game.load.image("playbutton", "assets/sprites/playbutton.png");
+		game.load.image("backsplash", "assets/sprites/backsplash.png");
 	},
 	create: function(){
 		game.state.start("TitleScreen");
@@ -45,12 +46,21 @@ preload.prototype = {
 var titleScreen = function(game){};
 titleScreen.prototype = {
 	create: function(){
+		var titleBG = game.add.tileSprite(0, 0, game.width, game.height, "backsplash");
+		titleBG.tint = bgColors[game.rnd.between(0,bgColors.length-1)];
 		//set random background color.
-		game.stage.backgroundColor = bgColors[game.rnd.between(0,bgColors.length-1)];
+		//game.stage.backgroundColor = bgColors[game.rnd.between(0,bgColors.length-1)];
 		var title = game.add.image(game.width/2, 210, "title");
+		//title.tint = bgColors[game.rnd.between(0,bgColors.length-1)];
 		title.anchor.set(0.5);
 		var playButton = game.add.button(game.width/2, game.height-150,"playbutton", this.startGame);
 		playButton.anchor.set(0.5);
+		var playButtonTween = game.add.tween(playButton).to({
+			width:220,
+			height:220
+		}, 1500, "Linear", true, 0, -1);
+		playButtonTween.yoyo(true);
+		
 	},
 	startGame: function(){
 		game.state.start("PlayGame");
