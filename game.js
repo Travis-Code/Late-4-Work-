27c166a2cea1c5e86b161e50d62b8b5edf1af70a
Trav <jorkilla@gmail.com>
2016-02-09 21:38:40 -1000

    More Customization
    tweaked variables and added cursing, also added more sounds

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 var game;
+var curse = ["Watch it you suka", "Whoops, MY BAD!", "Outta MY way!","Not my fault","chee hee!","Aisus!"];
 var bgColors = [0xF16745, 0xFFC65D, 0x7BC8A4, 0x4CC3D9, 0x93648D, 0x7c786a,
 0x588c73, 0x8c4646, 0x2a5b84, 0x73503c];
 var titleColors = [0xF16745, 0xFFC65D, 0x7BC8A4, 0x4CC3D9, 0x93648D, 0x7c786a];
@@ -6,18 +7,16 @@ var tunnelWidth = 256;
 var shipHorizonalSpeed = 100;
 //shipMoveDelay: stop movement of ship while in tween.
 var shipMoveDelay = 0;
-var shipVerticalSpeed = 15000000;
+var shipVerticalSpeed = 500000;//15000000;
 //swipeDistance tells us any swipe movement greater than 10 pixels will be
 //considered a swipe.
 var swipeDistance = 10;
 //var barrierSpeed = Math.random()*1000+30;
-var barrierSpeed = 280;
-var barrierGap = 190;
+var barrierSpeed = 480;
+var barrierGap = 200;
 var shipHealth = 1;
 var barrierIncreaseSpeed = 1.03;
 var tunnelBGSpeed = 900;
-
-
 
 
 //our custom barrier class.
@@ -56,8 +55,8 @@ Barrier = function(game, speed, tintColor){
 		}
 		if(this.y > game.height){
 			this.destroy();
-		}
 
+		} 
 	}
 };
 
@@ -71,7 +70,6 @@ Barrier.prototype.constructor = Barrier;
 
 window.onload = function() {	
 	game = new Phaser.Game(640, 960, Phaser.AUTO, ""); 
-	//add states
 	game.state.add("Boot", boot);
 	game.state.add("Preload", preload);
 	game.state.add("TitleScreen", titleScreen);
@@ -109,7 +107,7 @@ preload.prototype = {
 		game.load.image("tunnelbg", "assets/sprites/roadSpriteSide.png");
 		game.load.image("wall","assets/sprites/grassTile.png");
 		game.load.image("ship", "assets/sprites/scionTopView.png");
-		game.load.image("smoke", "assets/sprites/smoke.png");
+		game.load.image("smoke", "assets/sprites/smoke2.png");
 		game.load.image("barrier", "assets/sprites/shitVic.png");
 		game.load.audio("bgmusic", ["assets/sounds/bgmusic.mp3","assets/sounds/bgmusic.ogg"]);
 		game.load.audio("explosion", ["assets/sounds/explosion.mp3","assets/sounds/explosion.ogg"]);
@@ -117,6 +115,7 @@ preload.prototype = {
 		game.load.audio("carStart", ["assets/sounds/carStart.mp3","assets/sounds/carStart.ogg"]);
 		game.load.audio("carCrash", ["assets/sounds/Crash.mp3","assets/sounds/Crash.ogg"]);
 		game.load.audio("honk", ["assets/sounds/honk.mp3","assets/sounds/honk.ogg"]);
+		game.load.audio("screech", ["assets/sounds/screech.mp3","assets/sounds/screech.ogg"]);
 
 	},
 	create: function(){
@@ -163,9 +162,12 @@ titleScreen.prototype = {
 		
 	},
 	startGame: function(){
-
+				game.time.events.add(Phaser.Timer.SECOND * 1, function(){
+					console.log("it werks");
+					this.fade("PlayGame");
+				}, this);
 				console.log("switching to play game state");
-				this.fade("PlayGame");
+				//this.fade("PlayGame");
 		//game.state.start("PlayGame");
 	},
 
@@ -304,12 +306,19 @@ playGame.prototype = {
 	moveShip: function(){
 		this.ship.canSwipe = true;
 		if(this.ship.canMove && shipHealth <= 3){
+			var explosionSound = game.add.audio("screech");
+				explosionSound.play();
 			//set canMove to false so to stop the method from repeat firing while moveShip method is executing.
 			this.ship.canMove = false;
 			//if ship.side is 1 then turns to 0, if ship.side is 0 then turns to 1.
 			this.ship.side = 1 - this.ship.side;
-			this.ship.angle = 30 - this.ship.angle;
-			this.ship.angle = 0;
+			if(this.ship.side ==1){
+				this.ship.angle = 30;
+			}
+			else{			
+				this.ship.angle = -30;
+				}
+			//this.ship.angle = 0;
 			//make tween on ship that moves it from the current side to the opposite side.
 			var horizontalTween = game.add.tween(this.ship).to({
 				x:this.shipPositions[this.ship.side]
@@ -321,6 +330,7 @@ playGame.prototype = {
 			horizontalTween.onComplete.add(function(){
 				game.time.events.add(shipMoveDelay, function(){
 					this.ship.canMove = true;
+					this.ship.angle = 0;
 			//"this" refers to playGame object.
 				}, this);
 			}, this);
@@ -372,6 +382,9 @@ playGame.prototype = {
 		//after the explosion the ship will call game.state.start("GameOverScreen");
 		// and will switch to the game over state.
 
+
+
+
 		if(!this.ship.destroyed){
 			game.physics.arcade.collide(this.ship, this.barrierGroup, function(s,b)
 			{
@@ -380,20 +393,35 @@ playGame.prototype = {
 				var explosionSound = game.add.audio("explosion");
 				explosionSound.play();
 				this.addQuake();
-				console.log("watch it!");
+				var text = game.add.text(game.width/2, game.world.centerY+100, curser, style);
+				var curser = curse[game.rnd.between(0,curse.length-1)];
+				var style = {font: "65px Impact", fill: "#ffffff", align: "center"}
+				var text = game.add.text(game.width/2, game.world.centerY+100, curser, style);
+				text.anchor.set(0.5);
+
+				if(barrierSpeed == 800){
+					game.time.events.add(Phaser.Timer.SECOND * 0.2, function(){
+						text.destroy();
+					}, this);
+				}
+				else{
+					game.time.events.add(Phaser.Timer.SECOND * 0.5, function(){
+						text.destroy();
+					}, this);
+
+				}
+
 				var carHonk = game.add.audio("honk");
 				carHonk.play();
 
 
-
-					
 			if(shipHealth >= 4){
 				this.ship.destroyed = true;
 				this.smokeEmitter.destroy();
 				var destroyTween = game.add.tween(this.ship).to({
 					x: this.ship.x + game.rnd.between(-100, 100),
-					y: this.ship.y -100,
-					rotation: 10
+					y: this.ship.y + game.rnd.between(-500, 800),
+					rotation: 20
 				}, 1000, Phaser.Easing.Linear.None, true);
 				destroyTween.onComplete.add(function(){
 					this.bgMusic.stop();
@@ -407,13 +435,13 @@ playGame.prototype = {
 					var carCrash = game.add.audio("carCrash");
 					carCrash.play();
 					this.ship.destroy();
-					//this.fade("GameOverScreen");
-
+					//this.ship.y = 100;
 					game.time.events.add(Phaser.Timer.SECOND * 2, function(){
-					shipHealth = 1;
+					this.ship.destroy();
+
 					//game.state.start("GameOverScreen");
 					this.fade("GameOverScreen");
-
+					this.ship.destroy();
 					}, this);
 				}, this);
 			}
@@ -452,25 +480,19 @@ playGame.prototype = {
 		s.start();    
 	},
 
-
-
-
-
-
 	//restartShip method will switch stop any interaction with user until it is completed.
 	//it will stop the current verticalTween tween.
 	//and change the value of the ships verticalTween from y: 0 to y: 860 (back to the original y: origin of the ship)
 	//then once the change to the position of 860 to y is complete it will start
 	//the tween of the ship back to y: 0 again.
 	restartShip: function(){
-
 		if(!this.ship.destroyed && this.ship.alpha ==1){
 			barrierSpeed *= barrierIncreaseSpeed;
 			for(var i = 0; i < this.barrierGroup.length; i++){
 				this.barrierGroup.getChildAt(i).body.velocity.y = barrierSpeed;
 			}
-			if (barrierSpeed >= 400){
-				barrierSpeed = 400;
+			if (barrierSpeed >= 980){
+				barrierSpeed = 890;
 			}
 		}
 
@@ -478,7 +500,8 @@ playGame.prototype = {
 		this.verticalTween.stop();
 		this.verticalTween = game.add.tween(this.ship).to({
 			y: 860
-		}, 100, Phaser.Easing.Linear.None, true);
+		}, 
+		100, Phaser.Easing.Linear.None, true);
 		this.verticalTween.onComplete.add(function(){
 			this.verticalTween = game.add.tween(this.ship).to({
 				y: 0
@@ -493,7 +516,7 @@ playGame.prototype = {
 	},
 
 
-	addQuake: function() {
+	addQuake: function(){
 		// define the camera offset for the quake
 		var rumbleOffset = 10;
 		// we need to move according to the camera's current position
@@ -517,18 +540,41 @@ playGame.prototype = {
 		//quake.onComplete.addOnce(addQuake);
 		// let the earthquake begins
 		quake.start();
-}
+},
 
-
-
-
+addCarShake: function(){
+		// define the camera offset for the quake
+		var rumbleOffset = 10;
+		// we need to move according to the camera's current position
+		var properties = {
+		  x: ship.x - rumbleOffset
+		};
+		// we make it a relly fast movement
+		var duration = 100;
+		// because it will repeat
+		var repeat = 4;
+		// we use bounce in-out to soften it a little bit
+		var ease = Phaser.Easing.Bounce.InOut;
+		var autoStart = false;
+		// a little delay because we will run it indefinitely
+		var delay = 0;
+		// we want to go back to the original position
+		var yoyo = true;
+		var quake = game.add.tween(ship.x)
+		  .to(properties, duration, ease, autoStart, delay, 4, yoyo);
+		// we're using this line for the example to run indefinitely
+		//quake.onComplete.addOnce(addQuake);
+		// let the earthquake begins
+		quake.start();
+	}
 }
 
 var gameOverScreen = function(game){};
 gameOverScreen.prototype = {
 	create: function(){
+		shipHealth = 1;
 		var gameOverBG = bgColors[game.rnd.between(0,bgColors.length-1)];
-		var style = {font: "65px Helvetica", fill: "#ff0044", align: "center"}
+		//var style = {font: "65px Helvetica", fill: "#ff0044", align: "center"}
 		game.stage.backgroundColor = gameOverBG;
 		//var text = game.add.text(game.width/2, game.world.centerY+100, "Again?", style);
 		//text.anchor.set(0.5);
@@ -537,6 +583,7 @@ gameOverScreen.prototype = {
 		var title = game.add.image(game.width/2, 310, "ragetitle");
 		title.tint = titleColors[game.rnd.between(0,titleColors.length-1)];
 		title.anchor.set(0.5);
+
 		var titleTween = game.add.tween(title).to({
 			width:420,
 			height:420
@@ -556,18 +603,20 @@ gameOverScreen.prototype = {
 		//if yoyo method is set to false it will repeat without reversing.
 		playButtonTween.yoyo(true);
 
-	},
+},
+
+		
 	startGame: function(){
 		var carHonk = game.add.audio("honk");
 		carHonk.play();
 		//reset barrierSpeed back to 280 for next game.
-		barrierSpeed = 280;
+		barrierSpeed = 480;
 		this.fade("PlayGame");
 		//game.state.start("PlayGame");
 	},
 
-		//fade state method.
-		fade: function (nextState){
+		//fade to a different state method.
+	fade: function (nextState){
 		var spr_bg = this.game.add.graphics(0, 0);        
 		spr_bg.beginFill(this.fadeColor, 1);        
 		spr_bg.drawRect(0, 0, this.game.width, this.game.height);        
@@ -584,6 +633,7 @@ gameOverScreen.prototype = {
 		this.game.state.start(this.nextState);        
 		this.fadeOut();    
 	},    
+
 	fadeOut: function (){        
 		var spr_bg = this.game.add.graphics(0, 0);        
 		spr_bg.beginFill(this.fadeColor, 1);        
